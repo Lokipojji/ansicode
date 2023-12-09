2b59cbda75ca90bc3315850f40531f31eacc7edd
Lokipojji <153291144+Lokipojji@users.noreply.github.com>
2023-12-09 17:03:11 +0530

    this is scripts changes

---
+++ prg1.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+a=0
+n=0
+t=0
+exec < c.txt
+while read host
+do
+  ping -c 2 $host >/dev/null 2>&1
+  if [ $? -eq 0 ]
+  then
+    echo "$host is alive"
+    ((a++))
+  else
+    echo "$host not alive"
+    ((n++))
+  fi
+  ((t++))
+done
+ echo "summary
+----------------
+server alive: $a
+server Not alive: $n"
+#End
+
